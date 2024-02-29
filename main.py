89001373464abeef347630fcde55e332e1c722be
281developer <dave.cowden@gmail.com>
2024-02-29 16:55:19 -0500

    basic version working

--- main.py
+++ main.py
@@ -1,50 +1,44 @@
 from nicegui import ui,run
 from dulwich.repo import Repo
 from dulwich import porcelain
+import asyncio
+import os.path
 import subprocess
-
-REPO_PATH='/home/photonvision/gitwork/Robot2024/'
+import platform
+import shlex
+import sys
+REPO_PATH='c:/users/davec/gitwork/Robot2024/'
 
 repo = Repo(REPO_PATH)
-result_text=""
+
+log_pane=None
 
 def list_branches():
-    r =  porcelain.branch_list(repo)
+    r = porcelain.branch_list(repo)
     print(r)
     return r
-    
-@ui.refreshable
-def build_output_ui():
-     global result_text
-     print("update ui, now result_text=",result_text)
-     with ui.row():
-        ui.html("Results:<pre>\n" + str(result_text) + "</pre>")
-    
-    
-def run_build(gradle_command):
-    global result_text
-    cmd =  [ 'ls' , '-l']
-    #cmd = ["gradlew",gradle_command ]
-    result_text = subprocess.check_output(cmd, cwd=REPO_PATH, shell=True).decode()
-    print ("Done, result=")
-    print ( result_text)
-    build_output_ui.refresh()
 
-async def handle_run_build():
-    result= await run.cpu_bound(run_build,"build")
-    #build_output_ui.refresh()
+async def run_command(command: str) -> None:
+    """Run a command in the background and display the output in the pre-created dialog."""
+    process = await asyncio.create_subprocess_shell(command,
+        stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.STDOUT,
+        cwd=REPO_PATH
+    )
+    # NOTE we need to read the output in chunks, otherwise the process will block
+    output = ''
+    while True:
+        new = await process.stdout.read(200)
+        if not new:
+            break
+        output = new.decode()
+        # NOTE the content of the markdown element is replaced every time we have new output
+        log_pane.push(output)
 
 
-async def handle_run_deploy():
-    result= await run.cpu_bound(run_build,"deploy")
-    #build_output_ui.refresh()
-
-        
-ui.html("<h1>Gozer the Deployer</h1>")
-
-#with ui.row():
-ui.button('Deploy Main To Robot', on_click=handle_run_deploy)
-ui.button('Build Main', on_click=handle_run_build)
-#ui.separator()
-build_output_ui()
-ui.run()
+ui.label('Gozer the Deployer!').style('color: #367049; font-size: 100%; font-weight: 300')
+with ui.row():
+   ui.button('Build Main', on_click=lambda: run_command('.\gradlew build'))
+ui.separator()
+log_pane = ui.log(max_lines=500).classes('w-full h-80')
+ui.button('Clear Log', on_click=lambda: log_pane.clear() )
+ui.run(host="127.0.0.1", port=8081,reload=platform.system() != 'Windows')
