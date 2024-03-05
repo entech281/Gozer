8a274f3f86d405e2fc3502427ec3ee4d65106011
281developer <dave.cowden@gmail.com>
2024-03-04 19:52:30 -0500

    addd logging, and failed red text when build doesnt work

--- git_commands.py
+++ git_commands.py
@@ -2,6 +2,8 @@ import subprocess
 import asyncio
 import configparser
 import os.path
+import logging
+
 log_pane = None
 REPO_PATH=None
 CONFIG_FILE= "gozer.ini"
@@ -17,7 +19,10 @@ if not os.path.exists(path_to_try):
     raise ValueError("Path '{s}' Does not appear to be valid in gozer.ini.sample".format(s=path_to_try))
 
 REPO_PATH = path_to_try
-print("Repository Path: ", REPO_PATH)
+DEPLOY_ENABLED = (config['DEFAULT']['enable_deploy'].lower() in ['true', '1', 't', 'y', 'yes'])
+
+logging.info("Repository Path: ", REPO_PATH)
+logging.info("Deploys Enabled:", DEPLOY_ENABLED)
 
 
 def set_log_pane(d):
@@ -36,8 +41,23 @@ def list_branches() -> list:
     else:
         return []
 
+async def run_deploy() -> bool:
+    log_pane.push("Running Gradle Build...")
+    await run_command('gradlew build')
+    logging.info("Running Gradle Build")
+    if DEPLOY_ENABLED:
+        log_pane.push("Running Gradle Deploy...")
+        successful_deploy = await run_command('gradlew deploy')
+    else:
+        successful_deploy = False
+        log_pane.push("Not Deploying: Deploys are disabled")
+
+    logging.info("Deploy Success:")
+    return successful_deploy
+
 def run_checkout(ref: str) -> str:
     log_pane.push("Pulling From Remote ".format(ref=ref))
+    logging.info("Pulling from remote...")
     fetch_result = subprocess.run(['git', 'fetch', '--all'], cwd=REPO_PATH, capture_output=True, shell=True,check=False)
     if fetch_result.returncode != 0:
         log_pane.push(fetch_result.stderr.decode())
@@ -45,13 +65,16 @@ def run_checkout(ref: str) -> str:
     else:
         log_pane.push(fetch_result.stdout.decode())
 
+    logging.info("Refreshing Branches...")
     log_pane.push("Refreshing Branches...".format(ref=ref))
     BRANCH_LIST = list_branches()
 
+    logging.info("Checking out ref '{ref}'".format(ref=ref))
     log_pane.push("Checking out ref '{ref}'".format(ref=ref))
     checkout_result=subprocess.run(['git','checkout', '-f', ref], cwd=REPO_PATH, capture_output=True,shell=True,check=False)
     git_hash=None
     if checkout_result.returncode == 0:
+        logging.info("Running git rev-parse --short HEAD")
         revparse = subprocess.run(['git','rev-parse','--short','HEAD'], cwd=REPO_PATH, capture_output=True)
         git_hash = revparse.stdout.decode()
 
@@ -60,20 +83,25 @@ def run_checkout(ref: str) -> str:
         log_pane.push("ERROR: {error}".format(error=checkout_result.stderr.decode()))
     return git_hash
 
-async def run_command(command: str) -> None:
+async def run_command(command: str) -> bool:
     """Run a command in the background and display the output in the pre-created dialog."""
+    logging.info("running command {s}".format(s=command))
     process = await asyncio.create_subprocess_shell(command,
         stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE,
         cwd=REPO_PATH
     )
     # NOTE we need to read the output in chunks, otherwise the process will block
     output = ''
+    total_output=''
     while True:
         new = await process.stdout.read(200)
         if not new:
             break
         output = new.decode()
+        total_output += output
         # NOTE the content of the markdown element is replaced every time we have new output
         log_pane.push(output)
+    logging.info("command completed.")
+    return total_output.find("FAILED") < 0
 
 BRANCH_LIST=list_branches()
--- ui.py
+++ ui.py
@@ -2,6 +2,8 @@ import platform
 from nicegui import ui, run, app
 from datetime import datetime
 import git_commands
+import logging
+import sys
 
 class AppKeys:
     BUILD_HIST="builds"
@@ -42,52 +44,68 @@ def nice_date(dt):
 @ui.refreshable
 def build_history() -> None:
     for gozer_build in get_saved_builds():
-        with BuildCard(gozer_build["id"]).classes('w-[600px] bg-gray-100') as card:
+        with BuildCard(gozer_build["id"]).classes('w-[600px] bg-gray-100 no-shadow border-[2px]') as card:
             with ui.row().classes():
                 with ui.column():
                     msg = 'from {branch} on {date}'.format(branch=gozer_build["branch"], date=gozer_build["date_built"])
-                    ui.label(gozer_build["id"]).classes('w-64 text-green-900').style('font-size: 200%; font-weight: bold')
+                    label_color = 'text-green-900'
+                    if gozer_build["success"]==True:
+                        label_color = 'text-green-900'
+                        button_label = 'Rebuild'
+                    else:
+                        button_label = 'Retry'
+                        label_color = 'text-red-900'
+
+                    ui.label(gozer_build["id"]).classes('w-64 '+label_color).style('font-size: 200%; font-weight: bold')
                     ui.label(msg).classes('w-64 text-gray-600').style('font-size: 120%; font-weight: bold')
-                with ui.column().classes('flow-root'):
-                    with ui.row().classes('w-64 float-right'):
-                        ui.button("Rebuild", on_click=lambda id=gozer_build["id"]: build_ref(card.build_id))
-                        ui.button("Delete ", on_click=lambda c=card: c.delete_card() ,color="red")
+                with ui.column().classes():
+                    with ui.row().classes('w-64 '):
+                        ui.button(button_label, on_click=lambda id=gozer_build["id"]: build_ref(card.build_id))
+                        ui.button("Delete ", on_click=lambda c=card: c.delete_card() ,color='red')
 
 
 async def build_ref(ref_to_build: str) -> None:
     log_pane.clear()
     git_hash = git_commands.run_checkout(ref_to_build)
     if git_hash is not None:
-        log_pane.push("Running Gradle Build...")
-        await git_commands.run_command('gradlew build')
+
+        success = await git_commands.run_deploy()
         add_build({
             "id": git_hash,
             "branch": ref_to_build,
-            "date_built": nice_date(datetime.now())
+            "date_built": nice_date(datetime.now()),
+            "success": success
         })
         log_pane.push("DONE")
 
 
 with ui.row().classes('w-full'):
-    ui.label('Gozer the Deployer!').style('font-size: 400%; font-weight: 300')
+    ui.label('Gozer the Deployer! v1.0').classes('text-green-800').style('font-size: 400%; font-weight: 200 color: #367049')
     ui.separator()
 with ui.row().classes('w-full no-wrap'):
     with ui.column().classes('w-1/3'):
        with ui.card().classes('w-60  bg-gray-100'):
-            ui.button('Build Main', on_click=lambda: build_ref('main'))
+            ui.button('Deploy Main', on_click=lambda: build_ref('main'))
        with ui.card().classes('w-60  bg-gray-100'):
-            ui.button('Build Custom Ref', on_click=lambda: build_ref(user_ref.value))
+            ui.button('Deploy Custom Ref', on_click=lambda: build_ref(user_ref.value))
             user_ref = ui.input('Enter Ref').props('clearable')
        with ui.card().classes('w-60  bg-gray-100'):
-            ui.button('Build Branch', on_click=lambda: build_ref(user_selected_ref.value))
+            ui.button('Deploy Branch', on_click=lambda: build_ref(user_selected_ref.value))
             user_selected_ref = ui.select(git_commands.BRANCH_LIST,value="main")
-    with ui.column().classes('w-2/3  bg-gray-100'):
+    with ui.column().classes('w-2/3 '):
         build_history()
 
 with ui.card().classes('w-full'):
-    ui.label('Logs').classes('w-full').style('font-color:black font-size: 80%; font-weight: bold')
-    log_pane = ui.log(max_lines=500).classes('w-full h-40').style('font-sise: 80%')
+    ui.label('Logs').classes('w-full').classes('text-green-800').style(' font-weight: bold')
+    log_pane = ui.log(max_lines=500).classes('w-full h-40').style('font-size: 80%')
     git_commands.set_log_pane(log_pane)
     ui.button('Clear Log', on_click=lambda: log_pane.clear())
 
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
 ui.run(host="127.0.0.1", port=8081,show=False,reload=platform.system() != 'Windows')
